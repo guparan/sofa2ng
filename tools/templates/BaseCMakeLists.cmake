538c7eb71875d6d76f572ce5e6027067470b0f5c
Guillaume Paran <guillaume.paran@sofa-framework.org>
2018-04-26 10:02:15 +0200

    EDIT CMakeLists template

--- tools/templates/BaseCMakeLists.cmake
+++ tools/templates/BaseCMakeLists.cmake
@@ -10,35 +10,29 @@ cheetahVarStartToken = autopack::
 ### and pass the path to your version in the cmake_template property of the project.
 
 cmake_minimum_required(VERSION 3.1)
-project(autopack::package_name VERSION 1.0 LANGUAGES CXX)
+project(autopack::package_name VERSION 1.0)
 
 #for depend in autopack::dependencies
 find_package(autopack::depend QUIET)
 #end for
 
-set(ALL_DEPENDENCIES_FOUND 1)
-#for depend in autopack::dependencies
-if(NOT autopack::{depend}_FOUND)
-    set(ALL_DEPENDENCIES_FOUND)
-endif()
-#end for
+set(HEADER_FILES config/${PROJECT_NAME}.h)
+set(SOURCE_FILES config/${PROJECT_NAME}.cpp)
 
-if(ALL_DEPENDENCIES_FOUND)
-
-set(HEADER_FILES
+list(APPEND HEADER_FILES
 #for filename in autopack::header_files   
     autopack::filename
 #end for
 )
 
-set(SOURCE_FILES
+list(APPEND SOURCE_FILES
 #for filename in autopack::source_files 
     autopack::filename
 #end for
 )
 
 set(EXTRA_FILES
-#for filename in autopack::extra_files   
+#for filename in autopack::extra_files
     autopack::filename
 #end for
 )
@@ -49,12 +43,9 @@ add_autopack::{package_type}(${PROJECT_NAME} ${HEADER_FILES} ${SOURCE_FILES} ${E
 add_autopack::{package_type}(${PROJECT_NAME} SHARED ${HEADER_FILES} ${SOURCE_FILES} ${EXTRA_FILES})
 #end if
 target_link_libraries(${PROJECT_NAME} PUBLIC autopack::{sorted_dependencies})
-target_compile_definitions(${PROJECT_NAME} PRIVATE "-DBUILD_TARGET_autopack::package_cname")
-target_include_directories(${PROJECT_NAME} PUBLIC "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src>")
-target_include_directories(${PROJECT_NAME} PUBLIC "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/deprecated_layout>")
+target_compile_definitions(${PROJECT_NAME} PRIVATE "-DBUILD_autopack::package_cname")
+target_include_directories(${PROJECT_NAME} PUBLIC "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/config>")
+target_include_directories(${PROJECT_NAME} PUBLIC "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/deprecated_layout>")
+target_include_directories(${PROJECT_NAME} PUBLIC "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/src>")
 
-install(TARGETS ${PROJECT_NAME} DESTINATION ${CMAKE_BINARY_DIR} EXPORT Find${PROJECT_NAME})
-export(TARGETS ${PROJECT_NAME} FILE "${CMAKE_BINARY_DIR}/cmake/Find${PROJECT_NAME}.cmake")
-else()
-    message(WARNING "There is missing dependencies. The autopack::{package_name} is then disabled")
-endif()
+sofa_create_package(${PROJECT_NAME} ${PROJECT_VERSION} ${PROJECT_NAME} ${PROJECT_NAME})
