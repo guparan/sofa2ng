53455b7220277e314b43048641aaad56aeeb63f5
Guillaume Paran <guillaume.paran@sofa-framework.org>
2018-07-26 10:55:16 +0200

    Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 
-# py -2 ./tools/mc.py recipes/Sofa.Component.Utils.rcpy ../.. ../../modules
-py -2 ./tools/mc.py recipes/Sofa.Helper.Bvh.rcpy ../.. ../../modules
+module="$1"
+
+py -2 ./tools/mc.py recipes/$module.rcpy ../.. ../../modules
